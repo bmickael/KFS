1bdcea11c684e1533479a1c36dffc4f6ce84dfc5
mordak <mordak@mordak-pc.debian>
2022-07-11 19:02:12 +0000

    Use local bindgen from crate.io on build script instead of fixed executable

--- rust_kernel/dependencies/libc_binding/build.rs
+++ rust_kernel/dependencies/libc_binding/build.rs
@@ -1,13 +1,34 @@
-use std::process::Command;
-
 fn main() {
     println!(r#"cargo:rerun-if-changed={}"#, "../../../libc/include");
     println!(r#"cargo:rerun-if-changed={}"#, "../../../libc/include/sys");
-    let out = Command::new("./bindgen.sh").output().unwrap();
 
-    if !out.status.success() {
-        panic!("{:?}", unsafe {
-            core::str::from_utf8_unchecked(&out.stdout)
-        });
-    }
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("all_includes.h")
+        .use_core()
+        .derive_default(true)
+        .layout_tests(false)
+        .impl_debug(true)
+        .ctypes_prefix("super")
+        .ignore_functions()
+        .blacklist_type("u8")
+        .blacklist_type("u16")
+        .blacklist_type("u32")
+        .clang_arg("--target=x86_64-pc-linux-gnu")
+        .clang_arg("--sysroot=/toolchain_turbofish/sysroot")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    bindings
+        .write_to_file("src/libc.rs")
+        .expect("Couldn't write bindings!");
 }
